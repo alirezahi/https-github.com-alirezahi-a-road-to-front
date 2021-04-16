7603ea8d94f328e7dde0e6fdecf126d23f8edadd
alireza heydari <alireza97hi@gmail.com>
2021-04-16 16:16:12 +0430

    fix(quote-exercise): change readme extension

--- exercises/quote/readme.md
+++ exercises/quote/readme.md
@@ -1,3 +1,3 @@
-Create a page to randomly show a Quote.
+Create a page to randomly show a Quote. (`Math, Math.random`)
 Add a button to the page to change the quote.(toggle button)
 Make sure that clicking on the toggle button won't show the same quote.
--- exercises/react/error-success-callback.js
+++ exercises/react/error-success-callback.js
@@ -1,4 +1,15 @@
+// regular function
 function response(text, error, success){
+    if(text.startsWith('Error')){
+        error();
+        return 400;
+    }
+    success();
+    return 200;
+}
+
+// arrow function
+var response = (text, error, success) => {
     if(text.startsWith('Error')){
         error();
         return 400;
