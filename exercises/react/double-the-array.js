6168f40d46c38db5daa2eb92428f73eda3f93457
alireza heydari <alireza97hi@gmail.com>
2021-04-16 15:00:08 +0430

    feat: add arrow function solution to double-the-array problem

--- exercises/react/double-the-array.js
+++ exercises/react/double-the-array.js
@@ -1,8 +1,19 @@
 var myArr = [1,2,3];
 
+
+// =========
+// regular function solution
 function double (result, item){
     console.log({result});
     return [...result, item, item];
 };
+
+
+// =========
+// arrow function solution
+var double = (result, item) => [...result ,item , item];
+
+
+// =========
 
 myArr.reduce(double, []);
